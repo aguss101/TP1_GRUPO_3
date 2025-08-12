4653951a5406038172a9799fdfc16c08f8aaeccf
Agustin <agustinbaied1@gmail.com>
2025-08-12 14:07:54 -0300

    First commit with the class Persona complete and Principal

---
+++ src/dominio/Persona.java
@@ -0,0 +1,58 @@
+package dominio;
+
+import java.time.LocalDate;
+
+public class Persona {
+	private String dni;
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaNacimiento;
+	private String genero;
+	private String direccion;
+	private String telefono;
+	private String email;
+	  
+	//Constructores
+	public Persona(String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero,
+			String direccion, String telefono, String email) {
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.genero = genero;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.email = email;
+	}
+
+	public Persona() { 
+		dni="0"; nombre="sin nombre"; apellido="sin apellido";
+		fechaNacimiento=LocalDate.of(1, 1, 1); genero="sin genero";
+		direccion="sin direccion"; telefono="sin telefono"; email="sin email";
+		}
+
+	public String getDni() {return dni;}
+	public String getNombre() { return nombre; }
+	public String getApellido() { return apellido; }
+	public LocalDate getFechaNacimiento() { return fechaNacimiento; }
+	public String getGenero() { return genero; }
+	public String getDireccion() { return direccion; }
+	public String getTelefono() { return telefono; }
+	public String getEmail() { return email; }	
+	
+	public void setDni(String dni) {this.dni = dni;}
+	public void setNombre(String nombre) { this.nombre = nombre; }
+	public void setApellido(String apellido) { this.apellido = apellido;}
+	public void setFechaNacimiento(LocalDate fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+	public void setGenero(String genero) { this.genero = genero; }
+	public void setDireccion(String direccion) { this.direccion = direccion; }
+	public void setTelefono(String telefono) { this.telefono = telefono; }
+	public void setEmail(String email) { this.email = email; }
+
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", fechaNacimiento="
+				+ fechaNacimiento + ", genero=" + genero + ", direccion=" + direccion + ", telefono=" + telefono
+				+ ", email=" + email + "]";
+	}
+}
---
+++ src/dominio/Principal.java
@@ -0,0 +1,20 @@
+package dominio;
+
+import dominio.Persona;
+import java.time.LocalDate;
+
+public class Principal {
+	public static void main(String[] args) {
+		Persona p = new Persona();
+		p.setDni("11232444");
+		p.setNombre("Jose");
+		p.setApellido("Rodolfo");
+		p.setFechaNacimiento(LocalDate.of(2002, 02, 02));
+		p.setGenero("Masculino");
+		p.setDireccion("Almirante 23");
+		p.setTelefono("1134345656");
+		p.setEmail("Rodolfo@gmail.com");
+		
+		System.out.println(p.toString());
+	}
+}
