cb60e291b4f31b9599beb1457d0263590a1b27c9
Nicole <nickybelu@gmail.com>
2025-08-12 17:53:27 -0300

    empleado class counter

--- src/dominio/Empleado.java
+++ src/dominio/Empleado.java
@@ -4,19 +4,23 @@ import java.time.LocalDate;
 import dominio.Persona;
 
 public class Empleado extends Persona {
-	private int legajo;
+	private int legajo = 1000;
 	private String puesto;
-	
+	private static int cont = 0;
 	
 	public Empleado() {
 		super();
+		cont++;
 		this.puesto = "Sin puesto";
+		this.legajo = cont;
 		
 	}
 	public Empleado(String dni, String nombre, String apellido, LocalDate fechaNacimiento, String genero,
 			String direccion, String telefono, String email,  String puesto) {
 		super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+		cont++;
 		this.puesto = puesto;
+		this.legajo = cont;
 		
 	}
 	
